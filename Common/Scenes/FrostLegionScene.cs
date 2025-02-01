f500d02040d6acb2b041d585a4ddbbc98398a3c6
GabeHasWon <gabriel@guevarameyer.com>
2025-01-31 20:10:36 -0600

    fix frost legion not working & re-export

--- Common/Scenes/FrostLegionScene.cs
+++ Common/Scenes/FrostLegionScene.cs
@@ -3,7 +3,7 @@
 internal class FrostLegionScene : ModSceneEffect
 {
     public override int Music => MusicLoader.GetMusicSlot(Mod, "Sounds/Music/FrostLegion");
-    public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
+    public override SceneEffectPriority Priority => SceneEffectPriority.BossLow;
     public override bool IsSceneEffectActive(Player player) => Main.invasionType == 2 && ModContent.GetInstance<MusicConfig>().FrostLegion 
         && player.ZoneOverworldHeight && Main.invasionProgressNearInvasion;
 }
