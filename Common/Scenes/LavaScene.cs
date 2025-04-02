238f6d949e9aa2f3ade5d99f766de316c98e005c
Abhay M <amanchala13@gmail.com>
2025-04-02 12:22:43 -0700

    Added Updated Marble OST

--- Common/Scenes/LavaScene.cs
+++ Common/Scenes/LavaScene.cs
@@ -6,7 +6,7 @@ internal class LavaScene : ModSceneEffect
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeMedium;
 
     public override bool IsSceneEffectActive(Player player) => ModContent.GetInstance<MusicConfig>().LavaLayer
-            && player.position.Y / 16 >= (Main.maxTilesY - (Main.maxTilesY * .25f))
+            && player.position.Y / 16 >= (Main.maxTilesY - (Main.maxTilesY * .33f))
             && !player.ZoneJungle
             && !player.ZoneCorrupt
             && !player.ZoneHallow
